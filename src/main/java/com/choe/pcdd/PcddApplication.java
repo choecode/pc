022591a4555c9d8750d6bee963914e43594f6c87
choe <446705260@qq.com>
2019-07-11 15:07:31 +0800

    init

---
+++ src/main/java/com/choe/pcdd/PcddApplication.java
@@ -0,0 +1,13 @@
+package com.choe.pcdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcddApplication.class, args);
+    }
+
+}
